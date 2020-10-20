463832fcdcb99e585be2152d0757174189547749
Nikka <nmofid@stanford.edu>
2020-10-19 21:25:54 -0700

    tiny fix to utils

--- utils.py
+++ utils.py
@@ -4,9 +4,9 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 
 def accuracies(Y, pred):
-    accuracy_train = accuracy_score(Y, pred)
+    accuracy = accuracy_score(Y, pred)
     f1 = f1_score(Y, pred)
     precision = precision_score(Y, pred)
     recall = recall_score(Y, pred)
-    return f1, precision, recall
+    return accuracy, f1, precision, recall
    
