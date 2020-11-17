7eb52efe31e4015efc327d122e2bcd00040e1ba9
Nikka <nmofid@stanford.edu>
2020-11-16 17:30:58 -0800

    logisitic regression fixes

--- logistic_regression.py
+++ logistic_regression.py
@@ -15,7 +15,7 @@ import spectralfeatures
 SF = 16000
 N_FFT = 512
 #N_FILES = 30
-N_FILES = 5401
+N_FILES = 5160
 
 def fourierTransform(filenames):
     fourierTransforms = []
@@ -30,7 +30,7 @@ def fourierTransform(filenames):
         S = spectralfeatures.spectrogram(file, sr=SF)
         S = S.flatten() #
         fourierTransforms.append(S)
-    return fourierTransforms
+    return np.array(fourierTransforms)
 
 
 def mfcc(filenames):
@@ -47,7 +47,7 @@ def mfcc(filenames):
         S = S.flatten() #
         S = S.flatten() #
         mfcc_list.append(S)
-    return mfcc_list
+    return np.array(mfcc_list)
 
 def mel(filenames):
     mfcc_list = []
@@ -61,7 +61,7 @@ def mel(filenames):
         S = S[:10, :]
         S = S.flatten() #
         mfcc_list.append(S)
-    return mfcc_list
+    return np.array(mfcc_list)
 
 def rms(filenames):
     mfcc_list = []
@@ -75,13 +75,13 @@ def rms(filenames):
         S = S[:20] # (100,), clipping the audio for logistic regression, clipping didnt help
         S = S.flatten() #
         mfcc_list.append(S)
-    return mfcc_list
+    return bp.array(mfcc_list)
     
-def logistic_regression(X_train, Y_train, X_dev, Y_dev,X_eval,Y_eval):
+def logistic_regression(feature, X_train, Y_train, X_dev, Y_dev,X_eval,Y_eval,X_eval_other,Y_eval_other):
     #import pdb
     #pdb.set_trace()
     clf = LogisticRegression(random_state=0,max_iter=1000).fit(X_train, Y_train)
-    dump(clf, 'models/LR/LR' + str(N_FILES) +'.joblib',compress=3)
+    dump(clf, 'models/LR/LR' + str(N_FILES) + feature+'.joblib',compress=3)
     pred_train = clf.predict(X_train)
     accuracy_train,f1_train,precision_train,recall_train = utils.accuracies(Y_train, pred_train)
     print("train accuracy:", accuracy_train)
@@ -104,8 +104,15 @@ def logistic_regression(X_train, Y_train, X_dev, Y_dev,X_eval,Y_eval):
     print("eval precision:", precision_eval)
     print("eval recall:", recall_eval)
     
+    pred_eval_other = clf.predict(X_eval_other)
+    accuracy_eval_other,f1_eval_other,precision_eval_other,recall_eval_other = utils.accuracies(Y_eval_other, pred_eval_other)
+    print("eval_other accuracy:", accuracy_eval_other)
+    print("eval_other f1:", f1_eval_other)
+    print("eval_other precision:", precision_eval_other)
+    print("eval_other recall:", recall_eval_other)
+    
+    
 def main():
-    print("Inside Main")
     parser = argparse.ArgumentParser()
         ## Required parameters
     parser.add_argument("--feature",
@@ -117,6 +124,7 @@ def main():
     feature = args.feature
     print("Feature", feature)
     X_trainfilenames, Y_train, X_devfilenames, Y_dev, X_evalfilenames, Y_eval = dataloader.load_data()
+    X_evalfilenames_other, Y_eval_other = dataloader.load_other_eval_data()
     
     #X_trainfilenames = X_trainfilenames[0:N_FILES]
     #Y_train = Y_train[0:N_FILES]
@@ -131,6 +139,8 @@ def main():
        print("X_dev finished")
        X_eval = np.stack(fourierTransform(X_evalfilenames))
        print("X_eval finished")
+       X_eval = np.stack(fourierTransform(X_evalfilenames_other))
+       print("X eval other finished")
     elif(feature == "dct"):
        features_x_train = dct_func(X_trainfilenames)
        X_train = np.stack(features_x_train) # (30, 10, 257) -> (30,), Gokul why do you use vstack - converting it to array instead
@@ -139,6 +149,8 @@ def main():
        print("X_dev finished")
        X_eval = np.stack(dct_func(X_evalfilenames))
        print("X_eval finished")
+       X_eval_other = np.stack(dct_func(X_evalfilenames_other))
+       print("X eval other finished")
     elif(feature == "mfcc"): 
        features_x_train = mfcc(X_trainfilenames)
        print("mfcc features shape",features_x_train.shape)
@@ -150,6 +162,8 @@ def main():
        print("X_dev features shape",X_dev.shape)
        X_eval = np.stack(mfcc(X_evalfilenames))
        print("X_eval finished")
+       X_eval_other = np.stack(mfcc(X_evalfilenames_other))
+       print("X eval other finished")
     elif(feature == "mel"):
        features_x_train = mel(X_trainfilenames)
        X_train = np.stack(features_x_train) # (30, 10, 257) -> (30,), Gokul why do you use vstack - converting it to array instead
@@ -158,15 +172,20 @@ def main():
        print("X_dev finished")
        X_eval = np.stack(mel(X_evalfilenames))
        print("X_eval finished")
+       X_eval_other = np.stack(mel(X_evalfilenames_other))
+       print("X eval other finished")
     elif(feature == "rms"):
        features_x_train =rms(X_trainfilenames)
        X_train = np.stack(features_x_train) # (30, 10, 257) -> (30,), Gokul why do you use vstack - converting it to array instead
        X_dev = np.stack(rms(X_devfilenames))
        X_eval = np.stack(rms(X_evalfilenames))
+       X_eval_other = np.stack(rms(X_evalfilenames_other))
+       print("X eval other finished")
     
    
-    logistic_regression(X_train, Y_train, X_dev, Y_dev,X_eval,Y_eval)
+    logistic_regression(feature,X_train, Y_train, X_dev, Y_dev,X_eval,Y_eval,X_eval_other,Y_eval_other)
     
 if __name__ == "__main__":
+    print("Inside Main")
     main()
     
